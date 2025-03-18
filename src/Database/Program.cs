741fb7caf6402ea888059bb93b73a18644ec41be
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-03-18 16:14:51 +0200

    Database

---
+++ src/Database/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database("database.db", 3);
+
+            database.CreateTable("users", new string[] { "id", "username", "password" });
+
+            database.ExecuteQuery("INSERT INTO users (id, username, password) VALUES (1, 'admin', 'admin')");
+
+            database.Close();
+        }
+    }
+}
